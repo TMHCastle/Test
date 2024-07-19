66ea8a35234db9309cdb3f4926202502da4cb603
unknown <719281404@qq.com>
2024-07-19 14:11:53 +0800

    Test ver.2024-07-19 14:11:53

--- 一键推送.sh
+++ 一键推送.sh
@@ -1,14 +1,10 @@
 now=$(date "+%Y-%m-%d %H:%M:%S")
 echo "Change Directory to D:/Git_G"
 cd D:/Git_G
-echo "Starting add-commit-pull-push..."
-<<<<<<< HEAD
+echo "开始add-commit-pull-push流程"
 git add .
 git commit -m "Test ver.$now"
 git pull git@github.com:EasyToFind1/Test.git
 git push git@github.com:EasyToFind1/Test.git
-=======
-git add . && git commit -m "Test ver.$now" && git pull git@github.com:EasyToFind1/Test.git && git push git@github.com:EasyToFind1/Test.git
->>>>>>> 08d33d7c905addfc48a42e96062964aa2f6376e7
-echo "Finish!"
+echo "推送成功！可以关闭了"
 read
